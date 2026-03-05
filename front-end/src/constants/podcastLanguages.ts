52bff86468ddbbf0ffaafddc9cd34376cc9b517b
sanbec234 <95863218+sanbec234@users.noreply.github.com>
2026-03-05 22:20:22 +0530

    languages are refined

--- front-end/src/constants/podcastLanguages.ts
+++ front-end/src/constants/podcastLanguages.ts
@@ -1,26 +1,7 @@
 export const INDIAN_PODCAST_LANGUAGES = [
   "English",
   "Hindi",
-  "Bengali",
-  "Telugu",
-  "Marathi",
   "Tamil",
-  "Urdu",
-  "Gujarati",
-  "Kannada",
-  "Odia",
-  "Malayalam",
-  "Punjabi",
-  "Assamese",
-  "Maithili",
-  "Santali",
-  "Kashmiri",
-  "Nepali",
-  "Konkani",
-  "Sindhi",
-  "Dogri",
-  "Manipuri",
-  "Bodo",
 ] as const;
 
 export const DEFAULT_PODCAST_LANGUAGE = "English";
